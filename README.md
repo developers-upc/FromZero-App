41eef0bc347d39edab4cac32451b325e7ddcd82b
Purak189 <106536136+Purak189@users.noreply.github.com>
2024-04-18 23:38:43 -0500

    Chore: Initial configuration

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# FromZero-App
+# GoCareer202302
+
+Aplicación para publicacion y seguimiento de un proyecto (publicado por una empresa) por parte de un desarrollador freelance.
+
+Desarrollado por la Start-up SiteForge usando elementos Angular para el frontend
+
+Equipo de trabajo:
+- Velasquez Pizarro, Jair
+- Quilla Luyo, Jorge Gerardo
+- Ticona Luque, Rony Piero
+- Carrasco Godos, Juan Fernando
---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HttpClientModule} from "@angular/common/http";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatCardModule} from "@angular/material/card";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatFormFieldModule} from "@angular/material/form-field";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatFormFieldModule
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
