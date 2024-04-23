1b738ff488f0073f68952936935b5864299380cf
JorgeQuilla <g.quillaluyo@gmail.com>
2024-04-23 12:43:58 -0500

    feat: responsive sidenav added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FromZero-App';
+
+  isSidenavExpanded=false;
+  screenWidth=0;
+
+  onToggleSidenav(data:any){
+    this.screenWidth=data.screenWidth;
+    this.isSidenavExpanded = data.expanded;
+    console.log(this.screenWidth);
+  }
+
+  getContentClass():string{
+    let styleClass='';
+    if(this.isSidenavExpanded && this.screenWidth >768 ) {
+      styleClass = 'content-trimmed';
+    }
+    return styleClass;
+  }
 }
