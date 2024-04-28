45ac671a00ba086509393f19958c28af467eac0b
Purak189 <106536136+Purak189@users.noreply.github.com>
2024-04-27 21:46:09 -0500

    feat: adding basic components

--- src/app/pages/home/home-enterprise/components/home-profile/home-profile.component.ts
+++ src/app/pages/home/home-enterprise/components/home-profile/home-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {IEnterpriseProfile} from "../../models/enterprise-profile.model";
 
 @Component({
   selector: 'app-home-profile',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './home-profile.component.css'
 })
 export class HomeProfileComponent {
-
+  perfilUsuario: IEnterpriseProfile = {
+    name: 'FromZero',
+    description: 'A startup that helps other startups to grow',
+    country: 'Brazil',
+    cellphone: '+55 11 99999-9999',
+    email: 'kukuku@upc.pe',
+    website: 'https://fromzero.com',
+    image: 'https://geekitpe.com/wp-content/uploads/2022/11/152x152.jpg',
+    sector: 'Technology',
+    projects: [],
+  }
 }
