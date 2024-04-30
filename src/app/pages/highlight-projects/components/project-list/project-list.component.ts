6cedd8ca26450ba54cf5b84596cc4a2b64a6b1d0
jufer10 <fcarrasco023@gmail.com>
2024-04-30 09:36:06 -0500

    feat: highlight-project parte visual completada

--- src/app/pages/highlight-projects/components/project-list/project-list.component.ts
+++ src/app/pages/highlight-projects/components/project-list/project-list.component.ts
@@ -12,21 +12,24 @@ export class ProjectListComponent {
       title:"Pagina de ajedrez",
       description: "Este entregable consistirá en el código fuente del frontend y backend de la Plataforma de Comercio Electrónico Geekit. Se proporcionará una estructura de directorios organizada, con comentarios claros y limpios en el código para facilitar la comprensión y el mantenimiento futuro.",
       type: "Aplicacion Web",
-      stars: 5
+      stars: [1,2,3,4,5],
+      image_url:"/assets/highlight-projects-images/ajedrez.png"
     },
     {
       id: 2,
       title:"Reproduccion de Peliculas",
       description: "Este entregable consistirá en el código fuente del frontend y backend de la Plataforma de Comercio Electrónico Geekit. Se proporcionará una estructura de directorios organizada, con comentarios claros y limpios en el código para facilitar la comprensión y el mantenimiento futuro.",
       type: "Aplicacion Movil",
-      stars: 5
+      stars: [1,2,3,4,5],
+      image_url:"/assets/highlight-projects-images/peliculas.png"
     },
     {
       id: 3,
       title:"Proyecto Productividad en Entel",
       description: "Se entregará la Plataforma de Comercio Electrónico Geekit completamente desplegada y funcionando en un entorno de producción. Esto incluirá el hosting del sitio web, la configuración de la base de datos, la implementación de medidas de seguridad y cualquier otro aspecto necesario para que la plataforma esté lista para su lanzamiento y uso por parte de los usuarios finales.",
       type: "Data Analytics",
-      stars: 5
+      stars: [1,2,3,4,5],
+      image_url:"/assets/highlight-projects-images/entel.png"
     }
   ]
 }
