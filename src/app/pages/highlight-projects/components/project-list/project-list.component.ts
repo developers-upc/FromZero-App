6508b765f7b21ab114004ae90a6e282dcb5b307a
jufer10 <fcarrasco023@gmail.com>
2024-04-30 10:04:32 -0500

    feat: implementacion de filtros

--- src/app/pages/highlight-projects/components/project-list/project-list.component.ts
+++ src/app/pages/highlight-projects/components/project-list/project-list.component.ts
@@ -27,9 +27,31 @@ export class ProjectListComponent {
       id: 3,
       title:"Proyecto Productividad en Entel",
       description: "Se entregará la Plataforma de Comercio Electrónico Geekit completamente desplegada y funcionando en un entorno de producción. Esto incluirá el hosting del sitio web, la configuración de la base de datos, la implementación de medidas de seguridad y cualquier otro aspecto necesario para que la plataforma esté lista para su lanzamiento y uso por parte de los usuarios finales.",
-      type: "Data Analytics",
+      type: "Analisis de Datos",
       stars: [1,2,3,4,5],
       image_url:"/assets/highlight-projects-images/entel.png"
-    }
+    },
+    {
+      id: 4,
+      title:"Copia de Prueba",
+      description: "Este entregable consistirá en el código fuente del frontend y backend de la Plataforma de Comercio Electrónico Geekit. Se proporcionará una estructura de directorios organizada, con comentarios claros y limpios en el código para facilitar la comprensión y el mantenimiento futuro.",
+      type: "Aplicacion Web",
+      stars: [1,2,3,4],
+      image_url:"/assets/highlight-projects-images/ajedrez.png"
+    },
   ]
+
+  filteredProjects: any[] = [];
+
+  constructor() {
+    this.filteredProjects = this.projects;
+  }
+
+  filterProjects(type: string) {
+    if (type === "Borrar Filtros") {
+      this.filteredProjects = this.projects;
+      return;
+    }
+    this.filteredProjects = this.projects.filter(project => project.type === type);
+  }
 }
