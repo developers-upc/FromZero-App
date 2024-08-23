c9aaf461f21fa4b655dba475cc2a41336ee0fb79
JorgeQuilla <u20211b197@upc.edu.pe>
2024-08-23 17:47:25 -0500

    fix: deleting comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
-import { NgModule, Inject } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { ExampleDirective } from './shared/directives/example.directive';
 
 import {provideNativeDateAdapter} from "@angular/material/core";
 import {HttpClientModule} from "@angular/common/http";
@@ -15,7 +14,6 @@ import {SharedModule} from "./shared/shared.module";
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleDirective,
   ],
     imports: [
         BrowserModule,
